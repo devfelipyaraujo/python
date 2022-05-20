813b001b4328d39d0b3470468bec9889822760ce
devfelipyaraujo <devfelipyaraujo@gmail.com>
2022-05-19 22:48:51 -0300

    ex056

--- Projeto2/codigos/ex056.py
+++ Projeto2/codigos/ex056.py
@@ -1,22 +1,36 @@
 
 print(' ')
 
-sexm = 0
-sexf = 0
+maioridadehomem = 0
+nomevelho = ''
+somaidade = 0
+mediaidade = 0
+totmulher20 = 0
 
-for p in range(1, 6):
+for p in range(1, 5):
     print('----- {}ª Pessoa -----'.format(p))
-    nome = str(input('Nome: '))
+    nome = str(input('Nome: ')).strip()
     idade = int(input('Idade: '))
-    sexo = str(input('Sexo [M/F]: ')).upper()
-    
-    if sexo != 'M' and sexo != 'F':
-
-        print('Sexo Inválido')
-    
-
-
+    sexo = str(input('Sexo [M/F]: ')).upper().strip()
     print(' ')
-print('A média de idade do grupo é de {} anos'.format((idade*idade))/5)
+    somaidade += idade
+    
+    if p == 1 and sexo == 'M':
+        maioridadehomem = idade
+        nomevelho = nome
+    
+    if sexo == 'M' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    
+    if sexo == 'F' and idade < 20:
+        totmulher20 +=1    
+
+mediaidade = somaidade /4
+
+print(' ')
+print('A média de idade do grupo é de {} anos'.format(mediaidade))
+print('O homem mais velho tem {} anos e se chama {}'.format(maioridadehomem, nomevelho))
+print('Ao todo são {} mulheres com menos de 20 anos'.format(totmulher20))
 
   
